3f230f96029f0a0b4a2da0e822b827bbd29fe60b
ByungChulKang <bckang92@naver.com>
2021-02-22 23:40:28 +0900

    first commit

---
+++ demoPjt/src/main/java/com/react_jpa/demoPjt/DemoPjtApplication.java
@@ -0,0 +1,13 @@
+package com.react_jpa.demoPjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPjtApplication.class, args);
+	}
+
+}
---
+++ demoPjt/src/test/java/com/react_jpa/demoPjt/DemoPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.react_jpa.demoPjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
